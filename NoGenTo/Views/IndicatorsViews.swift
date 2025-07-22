09c420ca322acf3258e3b66d2b4d615061d0d14d
Fernando Gabriel Martinez <gabrielmtzg76@gmail.com>
2025-07-21 21:15:31 -0600

    Agregar el mic al layout y acomodarlo a la izquierda

---
+++ NoGenTo/Views/IndicatorsViews.swift
@@ -0,0 +1,8 @@
+//
+//  IndicatorsViews.swift
+//  NoGenTo
+//
+//  Created by Fernando Gabriel Martinez on 20/07/25.
+//
+
+import Foundation
---
+++ NoGenTo/Views/MicInputView.swift
@@ -0,0 +1,8 @@
+//
+//  MicInputView.swift
+//  NoGenTo
+//
+//  Created by Fernando Gabriel Martinez on 21/07/25.
+//
+
+import Foundation
